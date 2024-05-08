6b2b68b885d1338309a7da22f012c5401bda9fa2
MAULANA TEGAR IBRAHIM <tegarmaulana973@gmail.com>
2024-05-08 22:51:19 +0700

    Image picker add action

--- app/meals/error.jsx
+++ app/meals/error.jsx
@@ -1,10 +1,10 @@
-"use client"
+"use client";
 
 export default function Error() {
-    return (
-        <main className="error">
-            <h1>An error occured</h1>
-            <p>failed to fetch data, please reload or try again later</p>
-        </main>
-    )
+  return (
+    <main className="error">
+      <h1>An error occured!</h1>
+      <p>Failed to fetch meals data. Please reload or try again later.</p>
+    </main>
+  );
 }
